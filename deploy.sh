dce03cecae4f691390d4c39448dd837e0da64769
Colin Marc <colinmarc@gmail.com>
2017-02-23 17:44:53 +0100

    deploy new stuff

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 ssh nessus "sudo rm -rvf /var/www/*"
-tar cz $(find . -name "*.js" -or -name "*.html" -or -name "*.jpg") | ssh nessus "sudo tar xvz --no-same-owner -C /var/www"
+files=$(find . -name "*.js" -or -name "*.html" -or -name "*.css" -or -name "*.woff*" -or -name "*.jpg")
+tar cz $files | ssh nessus "sudo tar xvz --no-same-owner -C /var/www"
