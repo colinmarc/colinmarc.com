97f9d87bc5ddb42baeb69652dda81cbaef2bfeb9
Colin Marc <colinmarc@gmail.com>
2017-02-23 15:25:34 +0100

    Get generation working basically

--- dndspells/gen.js
+++ dndspells/gen.js
@@ -1,36 +1,17 @@
 var template = `
 {{#spells}}
-<div class="row">
-  {{#left}}
-  <div class="spell">
-    <h3><span class="printlevel">{{level}}</span> {{name}}</h3>
-    <b>Casting Time:</b> {{casting_time}}<br/>
-    <b>Range:</b> {{range}}<br/>
-    <b>Components:</b> {{component_desc}}<br/>
-    <b>Duration:</b> {{duration}}<br/>
-    <b>School:</b> {{school}}<br/>
-    {{{desc}}}
-    {{#higher_level}}
-    <p><b>At higher levels:</b></p>
-    {{{higher_level}}}
-    {{/higher_level}}
-  </div>
-  {{/left}}
-  {{#right}}
-  <div class="spell">
-    <h3><span class="printlevel">{{level}}</span> {{name}}</h3>
-    <b>Casting Time:</b> {{casting_time}}<br/>
-    <b>Range:</b> {{range}}<br/>
-    <b>Components:</b> {{component_desc}}<br/>
-    <b>Duration:</b> {{duration}}<br/>
-    <b>School:</b> {{school}}<br/>
-    {{{desc}}}
-    {{#higher_level}}
-    <p><b>At higher levels:</b></p>
-    {{{higher_level}}}
-    {{/higher_level}}
-  </div>
-  {{/right}}
+<div class="spell">
+  <h3>{{name}}</h3>
+  <div class="type">{{type}}</div>
+  <b>Casting Time:</b> {{casting_time}}<br/>
+  <b>Range:</b> {{range}}<br/>
+  <b>Components:</b> {{component_desc}}<br/>
+  <b>Duration:</b> {{duration}}<br/>
+  {{{desc}}}
+  {{#higher_level}}
+  <p><b>At higher levels:</b></p>
+  {{{higher_level}}}
+  {{/higher_level}}
 </div>
 {{/spells}}
 `;
@@ -38,30 +19,22 @@ var template = `
 Mustache.parse(template);
 
 var query = window.location.search.substring(1),
-    selected = new BitSet('0x' + query).toArray().sort().map(id => spells[id]),
-    spellbook = {spells: []};
+    spellbook = new BitSet('0x' + query).toArray().sort().map(id => spells[id]);
 
-selected.sort(function(a, b) {
+spellbook.sort(function(a, b) {
   if (a.level === b.level)
     return a.name < b.name ? -1 : 1;
   else
     return a.level - b.level;
-})
+});
 
-//     levels = [];
-
-// for (let sp of selected) {
-//   if (levels[sp.level] === undefined)
-//     levels[sp.level] = {level: sp.level, spells: [sp]}
-//   else
-//     levels[sp.level].spells.push(sp)
-// }
-
-while (selected.length > 1) {
-  spellbook.spells.push({left: selected.shift(), right: selected.shift()})
-  // TODO last spell
+for (let sp of spellbook) {
+  if (sp.level === 0)
+    sp.type = `${sp.school} Cantrip`
+  else
+    sp.type = `${sp.level_desc} ${sp.school}`
 }
 
 window.onload = function () {
-  document.getElementById('spells').innerHTML = Mustache.render(template, spellbook);
+  document.getElementById('spells').innerHTML = Mustache.render(template, {spells: spellbook});
 }
