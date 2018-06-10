156d541baa0023a133da1a463847a5eeeed301a3
Colin Marc <colinmarc@gmail.com>
2018-06-10 12:13:10 +0200

    Minify spells.js on the way up

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-(cd dndspells && echo -n "var spells = " > spells.js && cat spells.json >> spells.js)
+(cd dndspells && echo "var spells = " | cat - spells.json | minify --type js > spells.js)
 
 ssh nessus "sudo rm -rvf /var/www/*"
 files=$(find . -name "*.js" -or -name "*.html" -or -name "*.css" -or -name "*.woff*" -or -name "*.jpg")
